a26b19d1860248bb1c93081fd7eb40bd21c60cb9
Atlan <756777965@qq.com>
2023-03-30 15:36:06 +0800

    feat:compiler添加保存编译产物的属性

--- core/compiler.js
+++ core/compiler.js
@@ -15,6 +15,16 @@ class Compiler {
       // 在compiler 完成时执行 全部完成编译执行
       done: new SyncHook(),
     };
+    // 保存所有的入口模块对象
+    this.entries = new Set();
+    // 保存所有的依赖模块对象
+    this.modules = new Set();
+    // 保存所有的代码块对象
+    this.chunks = new Set();
+    // 存放本次产出的所有文件对象
+    this.assets = new Set();
+    // 存放本次编译所有产出的文件名
+    this.files = new Set();
   }
 
   // run方法启动编译
